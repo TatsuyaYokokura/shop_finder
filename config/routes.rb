f3b64c6a5bdfac6fa35fa19a10f9941e7bd5d4d5
横倉達也 <yokokuratatsuya@tatsuya.local>
2017-11-05 20:50:02 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :shops, only: :show do
-    resources :reviews, only: :create
+    resources :reviews, only: [:create]
   end
   root 'shops#index'
 end
